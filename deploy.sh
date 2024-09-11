9d19a2f148752b2f8b0870c1d0449caf3910efbe
Sjors Robroek <sjors@amazon.com>
2024-09-11 12:38:17 +0400

    updated deploys cript

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,11 @@ sudo cdk bootstrap
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 3600")
 AWS_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/region)
+
+test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || exit
+
+export CDK_DEFAULT_ACCOUNT=$ACCOUNT_ID
+export CDK_DEFAULT_REGION=$AWS_REGION
 EKS_ADMIN_ARN=arn:aws:iam::$ACCOUNT_ID:role/Admin
 #update the EKS ADMIN role to studio participant
 CONSOLE_ROLE_ARN=$EKS_ADMIN_ARN
