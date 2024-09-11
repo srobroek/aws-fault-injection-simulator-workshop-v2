c775989a0b5df710a1333218607d37e48a95171c
Sjors Robroek <sjors@amazon.com>
2024-09-11 11:37:28 +0400

    updated deploys cript

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo yum install git docker nodejs kubectl
+sudo yum install git docker nodejs
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x kubectl
+sudo mv kubectl /usr/local/bin
+
 sudo npm install -g aws-cdk
 git clone https://github.com/aws-samples/aws-fault-injection-simulator-workshop-v2
 sudo service docker start
