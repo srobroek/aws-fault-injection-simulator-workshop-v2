c69a7216afeb05df34786a89bfad89a23686592f
Sjors Robroek <sjors@amazon.com>
2024-09-15 12:57:38 +0400

    moved propagation of tags to cfn service

--- PetAdoptions/cdk/pet_stack/lib/services.ts
+++ PetAdoptions/cdk/pet_stack/lib/services.ts
@@ -338,10 +338,10 @@ export class Services extends Stack {
         })
         searchServiceEc2.taskDefinition.taskRole?.addToPrincipalPolicy(readSSMParamsPolicy);
         cdk.Tags.of(searchServiceEc2.taskDefinition).add('flag', '19c1f41b-c824-4a51-b858-757448aeca3e')
-        const cfnTaskDefinition = searchServiceEc2.taskDefinition.node.defaultChild as ecs.CfnTaskDefinition
-        cfnTaskDefinition.addPropertyOverride('PropagateTags', 'TASK_DEFINITION')
+        //const cfnTaskDefinition = searchServiceEc2.taskDefinition.node.defaultChild as ecs.CfnTaskDefinition
+        //cfnTaskDefinition.addPropertyOverride('PropagateTags', 'TASK_DEFINITION')
 
-        const cfnService = searchServiceEc2.service.node.defaultChild as ecs.CfnService
+        const cfnService = searchServiceEc2.service.node.findChild("Service") as ecs.CfnService
         cfnService.propagateTags = 'TASK_DEFINITION'
 /*        const cfnService = searchServiceEc2.service.node.defaultChild as ecs.CfnService
         cfnService.addPropertyOverride('PropagateTags', 'TASK_DEFINITION')*/
